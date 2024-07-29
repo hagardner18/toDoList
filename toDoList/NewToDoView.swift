ba71e8fb9505e11bab34fe8ca78b7d739f81eb72
Hannah Gardner <hagardner18@gmail.com>
2024-07-28 22:11:46 -0700

    Complete Iteration 0

---
+++ toDoList/NewToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  NewToDoView.swift
+//  toDoList
+//
+//  Created by Hannah Gardner on 7/28/24.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
