97c6a0385a35862d17525c51ebd8704b30674412
Hannah Gardner <hagardner18@gmail.com>
2024-07-28 21:27:40 -0700

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Hannah Gardner on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
